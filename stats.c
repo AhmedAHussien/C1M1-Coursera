521f0d12f8d868a1dda4829948a3b287ced51689
Ahmed Hessein <ahmed.a.hessein@gmail.com>
2018-12-16 00:31:20 +0200

    Add function definitions in stats.c & Fix data types used

--- stats.c
+++ stats.c
@@ -43,36 +43,145 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
 
 }
 
 /* Add other Implementation File Code Here */
-void print_statistics(int * ptr, int size)
+
+void print_statistics(unsigned char * ptr, unsigned char size)
 {
+  printf("\nStatistics of the data set");
+  printf("\n--------------------------------\n");
+  printf("Maximum element in the data set = %d\n", find_maximum(ptr, size));
+  printf("Minimum element in the data set = %d\n", find_minimum(ptr, size));
+  printf("Mean element in the data set = %d\n", find_mean(ptr, size));
+  printf("Median element in the data set = %d\n", find_median(ptr, size));
+  printf("--------------------------------\n");
+}
+
+void print_array(unsigned char * ptr, unsigned char size)
+{
+  unsigned char i = 0;
+  printf("\n--------------------------------\n");
+  printf("Elements of the array are:-\n");
+  for(i = 0; i < size; i++)
+  {
+    printf("Element of index %d = %d\n", i, ptr[i]);
+  }
+  printf("--------------------------------\n");
+}
+
+unsigned char find_median(unsigned char * ptr, unsigned char size)
+{
+  unsigned char index = 0;
+  unsigned char median = 0;
+  //array needs to be sorted before finding the median
+  sort_array(ptr, size);
+
+  if((size % 2) == 0)
+  {
+    // number of elements is even, median = mean of 2 middle elements
+    index = (size/2) - 1;
+    median = (ptr[index] + ptr[index+1]) / 2;
+  }
+  else
+  {
+    // number of elements is odd, median = the the middle element
+    index = (size/2);
+    median = ptr[index];
+  }
+}
+
+unsigned char find_mean(unsigned char * ptr, unsigned char size)
+{
+  /*
+  declaring a 16-bit integer variable that holds the sum of
+  elements in the array of 8-bit integer elements to avoid
+  overflow in some cases
+  */
+  unsigned short int sum = 0;
+  unsigned char mean = 0;
+  unsigned char i = 0;
+
+  for(i = 0; i < size; i++)
+  {
+    sum += ptr[i];
+  }
+
+  mean = sum/size;
+
+  return mean;
 
 }
 
-void print_array(int * ptr, int size)
+unsigned char find_maximum(unsigned char * ptr, unsigned char size)
 {
+  unsigned char maximum = 0;    // initialized with minimum value of unsigned char number
+  unsigned char i = 0;
 
+  for(i = 0; i < size; i++)
+  {
+    if(ptr[i] > maximum)
+    {
+      maximum = ptr[i];
+    }
+    else
+    {;}   //do nothing
+  }
+
+  return maximum;
 }
 
-int find_median(int * ptr, int size)
+unsigned char find_minimum(unsigned char * ptr, unsigned char size)
 {
+  unsigned char minimum = 0xFF;    // initialized with maximum value of unsigned char number
+  unsigned char i = 0;
 
+  for(i = 0; i < size; i++)
+  {
+    if(ptr[i] < minimum)
+    {
+      minimum = ptr[i];
+    }
+    else
+    {;}   //do nothing
+  }
+
+  return minimum;
 }
 
-int find_mean(int * ptr, int size)
+void sort_array(unsigned char * ptr, unsigned char size)
 {
+  unsigned char i = 0, j = 0;
+  unsigned char temp = 0;         // temporary variable used for swapping elements
+  unsigned char swap_flag = 0;    // a flag that is set when a swap is occured to reduce overhead
 
-}
+  for(i = 0; i < size-1; i++)
+  {
+    for(j = 0; j < size-i-1; j++)
+    {
+      if(ptr[j] < ptr[j+1])
+      {
+        //swap elements to bubble up the lesser value
+        temp = ptr[j];
+        ptr[j] = ptr[j+1];
+        ptr[j+1] = temp;
+        swap_flag = 1;
+      }
+      else
+      {;}   //do nothing
+    }
+    if(0 == swap_flag)
+    {
+      break;    // no swapping occured thus the array is sorted
+    }
+    else
+    {
+      swap_flag = 0;
+    }
+  }
 
-int find_maximum(int * ptr, int size)
-{
-
-}
-
-int find_minimum(int * ptr, int size)
-{
-  
 }
--- stats.h
+++ stats.h
@@ -2,15 +2,15 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file stats.h 
- * @brief Contains function declarations for 
+ * @file stats.h
+ * @brief Contains function declarations for
  * for statistics analysis
  *
  * This header file includes function declarations
@@ -33,95 +33,109 @@
  * @brief Prints the statistics of an array
  *
  * This function takes as input an address of an array
- * of 32-bit integer numbers and it's size in 32-bit
+ * of 8-bit integer numbers and it's size in 8-bit
  * integer. This will print the statistics of the array
  * which are minimum, mixmum, mean and median
  *
- * @param The 32-bit address of the array for its statistics to be printed
- * @param The 32-bit size of the array for its statistics to be printed
+ * @param The 8-bit address of the array for its statistics to be printed
+ * @param The 8-bit size of the array for its statistics to be printed
  *
  * @return void
  */
-void print_statistics(int * ptr, int size);
+void print_statistics(unsigned char * ptr, unsigned char size);
 
 
 /**
  * @brief Prints the elements of an array
  *
  * This function takes as input an address of an array
- * of 32-bit integer numbers and it's size in 32-bit
+ * of 8-bit integer numbers and it's size in 8-bit
  * integer. This will print the elements of the array
  *
- * @param The 32-bit address of the array to be printed
- * @param The 32-bit size of the array to be printed
+ * @param The 8-bit address of the array to be printed
+ * @param The 8-bit size of the array to be printed
  *
  * @return void
  */
-void print_array(int * ptr, int size);
+void print_array(unsigned char * ptr, unsigned char size);
 
 
 /**
  * @brief find the median of the elements of an array
  *
  * This function takes as input an address of an array
- * of 32-bit integer numbers and it's size in 32-bit
+ * of 8-bit integer numbers and it's size in 8-bit
  * integer. This will find the median of the elements
  * in the array
  *
- * @param The 32-bit address of the array to find the median value
- * @param The 32-bit size of the array to find the median value
+ * @param The 8-bit address of the array to find the median value
+ * @param The 8-bit size of the array to find the median value
  *
- * @return The 32-bit value of the median calculated
+ * @return The 8-bit value of the median calculated
  */
-int find_median(int * ptr, int size);
+unsigned char find_median(unsigned char * ptr, unsigned char size);
 
 
 /**
  * @brief find the mean of the elements of an array
  *
  * This function takes as input an address of an array
- * of 32-bit integer numbers and it's size in 32-bit
+ * of 8-bit integer numbers and it's size in 8-bit
  * integer. This will find the mean of the elements
  * in the array
  *
- * @param The 32-bit address of the array to find the mean value
- * @param The 32-bit size of the array to find the mean value
+ * @param The 8-bit address of the array to find the mean value
+ * @param The 8-bit size of the array to find the mean value
  *
- * @return The 32-bit value of the mean calculated
+ * @return The 8-bit value of the mean calculated
  */
-int find_mean(int * ptr, int size);
+unsigned char find_mean(unsigned char * ptr, unsigned char size);
 
 
 /**
  * @brief find the maximum of the elements of an array
  *
  * This function takes as input an address of an array
- * of 32-bit integer numbers and it's size in 32-bit
+ * of 8-bit integer numbers and it's size in 8-bit
  * integer. This will find the maximum of the elements
  * in the array
  *
- * @param The 32-bit address of the array to find the maximum value
- * @param The 32-bit size of the array to find the maximum value
+ * @param The 8-bit address of the array to find the maximum value
+ * @param The 8-bit size of the array to find the maximum value
  *
- * @return The 32-bit value of the maximum calculated
+ * @return The 8-bit value of the maximum calculated
  */
-int find_maximum(int * ptr, int size);
+unsigned char find_maximum(unsigned char * ptr, unsigned char size);
 
 
 /**
  * @brief find the minimum of the elements of an array
  *
  * This function takes as input an address of an array
- * of 32-bit integer numbers and it's size in 32-bit
+ * of 8-bit integer numbers and it's size in 8-bit
  * integer. This will find the minimum of the elements
  * in the array
  *
- * @param The 32-bit address of the array to find the minimum value
- * @param The 32-bit size of the array to find the minimum value
+ * @param The 8-bit address of the array to find the minimum value
+ * @param The 8-bit size of the array to find the minimum value
  *
- * @return The 32-bit value of the minimum calculated
+ * @return The 8-bit value of the minimum calculated
  */
-int find_minimum(int * ptr, int size);
+unsigned char find_minimum(unsigned char * ptr, unsigned char size);
 
+
+/**
+ * @brief sorts the elements of an array in descending order
+ *
+ * This function takes as input an address of an array
+ * of 8-bit integer numbers and it's size in 8-bit
+ * integer. This will sort the elements of the array
+ *
+ * @param The 8-bit address of the array for sorting
+ * @param The 8-bit size of the array for sorting
+ *
+ * @return void
+ */
+void sort_array(unsigned char * ptr, unsigned char size);
 
 #endif /* __STATS_H__ */
